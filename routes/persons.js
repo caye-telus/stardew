f554b4f3bc59ae6a5e2e5d74f320e556f38ed51f
Caye Borreo <caye.borreo+github@telusinternational.com>
2023-01-24 17:09:50 +0800

    feat(persons): add get by id, post

--- routes/persons.js
+++ routes/persons.js
@@ -6,7 +6,7 @@ let personsData = [
   id: 1,
   name: 'Emily',
   address: '2 Willow Lane'
-  },
+},
   {
     id: 2,
     name: 'Haley',
@@ -18,5 +18,23 @@ let personsData = [
 router.get('/', function(req, res, next) {
   res.send({ persons: personsData })
 });
+
+// GET by id
+router.get('/id/:personId', function(req, res, next) {
+  console.log(req.params.personId)
+  res.send({ persons: personsData.filter(person => person.id === parseInt(req.params.personId)) })
+});
+
+// POST
+router.post('/', function(req, res, next) {
+  personsData.push(req.body)
+  res.send({ persons: personsData })
+});
+
+// PATCH
+router.patch('/', function(req, res, next) {
+  let person = personsData.
+  res.send({ persons: personsData })
+})
 
 module.exports = router;
